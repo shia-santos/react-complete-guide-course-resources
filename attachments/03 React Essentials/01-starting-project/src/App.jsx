8353d5c3efc90a90ef5091ba72b708053a5cbb81
Sofia Santos <sofiabrandaosantos@gmail.com>
2025-04-21 16:58:24 +0100

    commit

---
+++ attachments/03 React Essentials/01-starting-project/src/App.jsx
@@ -0,0 +1,54 @@
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
+
+function App() {
+  function handleSelect() {
+    console.log("Hello World - selected!");
+  }
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {/* <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            /> */}
+            {/* 
+              -> fazer import la em cima primeiro: import componentsImg from "./assets/components.png"; 
+
+              <CoreConcept
+              title="Props"
+              description="The core UI building block."
+              image={componentsImg}
+            /> */}
+            <CoreConcept
+              {...CORE_CONCEPTS[0]} /* Alternativa mais curta à primeira, mais descritiva */
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
+          </menu>
+          Dynamic Content
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
